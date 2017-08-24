aeac20cdfb65e322e593a796129d305879f2058d
Naufal <naufal@JavascriptCoder.local>
2017-08-24 20:47:15 +0800

    Added secret and redis cloud

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,6 @@
 module.exports = {
   database: process.env.DATABASE || 'mongodb://naufal:abc123@ds153352.mlab.com:53352/newco', // URL will be change soon
+  // database: process.env.DATABASE || 'mongodb://localhost/blink',
   port: process.env.PORT || 3000,
   secret: process.env.SECRET || 'Naufal888**@#@###$$$%%199230',
   logging: {
--- modules/redis.js
+++ modules/redis.js
@@ -1,9 +1,12 @@
 const logger = require('./logger');
 const redis = require("redis");
 const client = redis.createClient({
-    host: global.config.redis.host || 'redis-10411.c15.us-east-1-2.ec2.cloud.redislabs.com', // For testing purposes
-    port: global.config.redis.port || 10411 // 
+    // host: global.config.redis.host || 'redis-10411.c15.us-east-1-2.ec2.cloud.redislabs.com', // For testing purposes
+    // port: global.config.redis.port || 10411 //
+    host: '127.0.0.1',
+    port: 6379
 });
+
 const geo = require('georedis').initialize(client);
 
 client.on('error', function (err) {
